90d137e2f0243bbefebd51d34e1a52849d18b8e2
Lippo <filippo.nardini@studio.unibo.it>
2020-09-18 01:10:12 +0200

    Update images

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 static_templates = {
-    'vote-title1': 'assets/vote-title1.png',
-    'vote-title2': 'assets/vote-title2.png',
-    'vote-arrow': 'assets/vote-arrow.png',
+	'chat-arrow': 'assets/chat-arrow.png',
+	'chat-arrow-message': 'assets/chat-arrow-message.png',
+	'chat-arrow-open': 'assets/chat-arrow-open.png',
+    # 'vote-title1': 'assets/vote-title1.png',
+    # 'vote-title2': 'assets/vote-title2.png',
+    # 'vote-arrow': 'assets/vote-arrow.png',
 }
 monitor = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
